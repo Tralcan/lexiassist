45e5fc5ab2769d595b7f741034a6d10b6a9c807e
Diego Anguita M. <tralcan@gmail.com>
2025-11-19 17:09:19 +0000

    ya descubrí el problema... la está pidiendo las variables: NEXT_PUBLIC_S

--- next.config.ts
+++ next.config.ts
@@ -31,10 +31,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    SUPABASE_SERVICE_ROLE_KEY: process.env.SUPABASE_SERVICE_ROLE_KEY,
-  }
 };
 
 export default nextConfig;
